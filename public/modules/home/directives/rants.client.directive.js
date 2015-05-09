c81323a81a20db7edd1408dedca61ea7d925d47e
Isaias Vena <isa@coolshop.it>
2015-05-09 14:56:29 +0200

    rants directives

---
+++ public/modules/home/directives/rants.client.directive.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('home').directive('rants', [
+	function() {
+		return {
+			templateUrl: 'modules/home/views/home.rants.html',
+			restrict: 'EA',
+			link: function postLink(scope, element, attrs) {
+				//TODO fetch this from resource
+				scope.rants = [
+                    {
+                        headline: "Hey die vuilniszakken he..., kap met die naast de container te zetten!!!!",
+                        date: "2015-05-01",
+                        signature: "Een buurtbewone"
+                    },
+                    {
+                        headline: "Wordt deze lantaarnpaal nog eens gefixt of wat?",
+                        date: "2015-03-15",
+                        signature: "Een bezorgde burger",
+                        upVotesCount: 14
+                    },
+                    {
+                        headline: "Misschien leuk om ook je vuurwerk eens op te ruimen. Vorig jaar ook al zo'n teringbende.",
+                        date: "2013-01-01"
+                    }
+                ]
+			}
+		};
+	}
+]);
